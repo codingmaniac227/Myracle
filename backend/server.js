bb5cc559a2d980a16b1ad0fc76e5fda960e59592
Coding <codingmaniac227@gmail.com>
2025-09-27 09:11:39 -0400

    feat(server): bootstrap Express app with security, logs, CORS, and route mount

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import helmet from "helmet";
-import cors from "cors";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import productRoutes from "./routes/productRoutes.js";
+import express from 'express'
+import helmet from "helmet"
+import cors from "cors"
+import morgan from "morgan"
+import dotenv from "dotenv"
+import productRoutes from "./routes/productRoutes.js"
+import { sql } from './config/db.js'
 
-dotenv.config();
+dotenv.config()
 const app = express()
 
 // Parses body req
@@ -16,14 +17,33 @@ app.use(cors({ origin: 'http://localhost:5173' }))
 
 // Middleware library. Prevents Cross-Site Scripting Attacks(XSS)
 app.use(helmet())
-app.use(morgan("dev")); // Log request to the console
+app.use(morgan("dev")) // Log request to the console
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 
 app.use('/api/products', productRoutes)
 
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price DECIMAL(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `
 
-app.listen(PORT, () => {
-    console.log(`Listening on port http://localhost:${PORT}`)
+        console.log("Database initialized")
+    } catch (error) {
+        console.error('Error initDB', error)
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`)
+    })
 })
