f9b99a2f873492e2f462646c504df1543978645e
Coding <codingmaniac227@gmail.com>
2025-09-30 23:40:27 -0400

    feat(server): configure middleware, Arcjet protection, DB init, and routes establish

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,37 @@ app.use(morgan("dev")) // Log request to the console
 
 const PORT = process.env.PORT
 
+// apply arcjet rate-limit to all routes
+
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, {
+            requested:1 // specifies that each request consumes 1 token
+        })
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()){
+                res.status(429).json({ error: 'Too Many Requests' })
+            } else if (decision.reason.isBot()){
+                res.status(403).json({ error: 'Bot Access Denied' })
+            } else {
+                res.status(403).json({ error: 'Forbidden' })
+            }
+            return
+        }
+        // check for spoofed bots
+        if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+            res.status(403).json({ error: 'Spoof Bot Detected' })
+            return
+        }
+
+        next()
+    } catch (error) {
+        console.log('Arcjet error', error)
+        next(error)
+    }
+
+})
 
 app.use('/api/products', productRoutes)
 
